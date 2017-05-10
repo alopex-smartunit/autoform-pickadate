9aed5390c01eaf5b0a1ff3762f8a6e64526f92ee
Андрей Лебедкин <duke@ymail.ru>
2017-05-11 07:35:08 +0900

    Remove hard dependencies.

--- package.js
+++ package.js
@@ -1,22 +1,22 @@
 Package.describe({
   name: "smartunit:autoform-pickadate",
-  version: "0.1.3",
-  summary: "pickadate.js for autoform (for AutoForm 6.0)",
+  version: "0.1.4",
+  summary: "pickadate.js for autoform",
   git: "https://github.com/alopex-smartunit/autoform-pickadate"
 });
 
 Package.onUse(function (api, where) {
-  api.versionsFrom('1.0.3.1');
+  api.versionsFrom('1.4');
 
   api.use('templating');
   api.use('underscore');
   api.use('jquery');
   api.use('tracker');
   api.use('coffeescript');
-  api.use('mquandalle:jade@0.4.1');
-  api.use('aldeed:autoform@6.0.0');
-  api.use('robertlowe:pickadate@3.5.5');
-  api.use('aldeed:template-extension@4.1.0');
+  api.use('mquandalle:jade');
+  api.use('aldeed:autoform');
+  api.use('robertlowe:pickadate');
+  api.use('aldeed:template-extension');
 
   api.addFiles('autoform-pickadate.jade', 'client');
   api.addFiles('autoform-pickadate.coffee', 'client');
